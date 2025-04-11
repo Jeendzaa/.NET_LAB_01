cc21afedda4143f501ebdc92e7ed24ce6ff21e06
Jendza <roj.jedrzej@outlook.com>
2025-04-11 14:32:38 +0200

    Dodaj pliki projektów.

---
+++ Zadanie_3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Zadanie_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" --== OODCZYT ARGUMENTÓW ==--");
+            if (args.Length == 0) Console.WriteLine("Brak argumentów");
+            else
+            {
+                foreach(string arg in args)
+                {
+                    Console.WriteLine($"{arg}");
+                }
+            }
+
+            Console.WriteLine(" --== OODCZYT ZMIENNYCH ==--");
+            foreach(var env in Environment.GetEnvironmentVariables().Keys)
+            {
+                Console.WriteLine($"{env} = {Environment.GetEnvironmentVariables()}");
+            }
+        }
+    }
+}
---
+++ Zadanie_4/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Zadanie_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string directory = args.Length > 0 ? args[0] : Directory.GetCurrentDirectory();
+            
+            if(!Directory.Exists(directory))
+            {
+                Console.WriteLine("Katalog nie isteniej");
+                return;
+            }
+
+            Console.WriteLine("Zawartość katalogu: ");
+            try
+            {
+                foreach(var dir in Directory.GetDirectories(directory))
+                {
+                    Console.WriteLine($"[FOLDER] {Path.GetFileName(dir)}");
+                }
+                
+                foreach(var file in Directory.GetFiles(directory))
+                {
+                    Console.WriteLine($"[PLIK] {Path.GetFileName(file)}");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"ERROR: {e.Message}");
+            }
+        }
+    }
+}
